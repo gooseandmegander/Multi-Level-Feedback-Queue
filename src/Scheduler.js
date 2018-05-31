f5dcdecf18eaaedc5a64dc79cb051f88c14f97f8
gooseandmegander <meganawilliamson@gmail.com>
2018-05-31 12:49:00 -0700

    Scheduler.js complete

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -26,29 +26,66 @@ class Scheduler {
   // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
   // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
   run() {
-    // while (true) {
-    //   const time = Date.now();
-    //   const workTime = time - this.clock;
-    //   this.clock = time;
-    //   if (!this.blockingQueue.isEmpty()) {
-    //     this.blockingQueue.doBlockingWork(workTime);
-    //   }
-    //   for (let i = 0; i < PRIORITY_LEVELS; i++) {
-    //     const queue = this.runningQueues[i];
-    //     if (!queue.isEmpty()) {
-    //       //
-    //     }
-    //   }
-    // }
+    // true until allQueuesEmpty returns true
+    while (!this.allQueuesEmpty()) {
+      // console.log('old clock: ', this.clock);
+      const time = Date.now();
+      // console.log('time: ', time);
+      // difference between current time and time when scheduler was created
+      const workTime = time - this.clock;
+      // console.log('workTime: ', workTime);
+      this.clock = time;
+      // console.log('new clock: ', this.clock);
+      if (!this.blockingQueue.isEmpty()) {
+        this.blockingQueue.doBlockingWork(workTime);
+      }
+      for (let i = 0; i < PRIORITY_LEVELS; i++) {
+        const queue = this.runningQueues[i];
+        if (!queue.isEmpty()) {
+          queue.doCPUWork(workTime);
+        }
+      }
+    }
   }
 
-  allQueuesEmpty() {}
+  allQueuesEmpty() {
+    for (let i = 0; i < this.runningQueues.length; i++) {
+      if (!this.runningQueues[i].isEmpty()) return false;
+    }
+    return true;
+  }
 
-  addNewProcess(process) {}
+  addNewProcess(process) {
+    this.runningQueues[0].enqueue(process);
+  }
 
   // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
   // Should handle PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY interrupts.
-  handleInterrupt(queue, process, interrupt) {}
+  handleInterrupt(queue, process, interrupt) {
+    // console.log('process: ', process);
+    // console.log('queue: ', queue);
+    switch (interrupt) {
+      case 'PROCESS_BLOCKED':
+        this.blockingQueue.enqueue(process);
+        break;
+      case 'PROCESS_READY':
+        this.addNewProcess(process);
+        break;
+      case 'LOWER_PRIORITY':
+        if (queue.getQueueType() === 'BLOCKING_QUEUE') {
+          this.blockingQueue.enqueue(process);
+        } else {
+          if (queue.getPriorityLevel() !== 2) {
+            this.runningQueues[queue.getPriorityLevel() + 1].enqueue(process);
+          } else {
+            this.runningQueues[2].enqueue(process);
+          }
+        }
+        break;
+      default:
+        return console.log('ERROR: Could not handle Interrupt');
+    }
+  }
 
   // Private function used for testing; DO NOT MODIFY
   _getCPUQueue(priorityLevel) {
